230c497853db2e13fc194ee995fcd7b18dee234c
EmileDeGamer <47303713+EmileDeGamer@users.noreply.github.com>
2020-02-15 10:45:28 +0100

    cleanup

--- js/script.js
+++ js/script.js
@@ -1,23 +1,5 @@
 let pageTitle = document.getElementById('pageTitle')
 
-let menuButtonsDisplay = document.getElementById('menuButtons')
-
-//let menuButtons = ['Login', 'Register', 'Logout']
-
-/*for (let i = 0; i < menuButtons.length; i++) {
-    let li = document.createElement('li')
-    let a = document.createElement('a')
-    a.innerHTML = menuButtons[i]
-    if(menuButtons[i] !== 'Login'){
-        a.href = menuButtons[i].toLowerCase() + ".php"
-    }
-    else{
-        a.href = "../pages"
-    }
-    li.appendChild(a)
-    menuButtonsDisplay.appendChild(li)
-}*/
-
 let showPassword = document.getElementById('showPassword')
 let showRepeatPassword = document.getElementById('showRepeatPassword')
 
@@ -29,45 +11,6 @@ if(showRepeatPassword !== null){
     makePasswordVisible('showRepeatPassword')
     showRepeatPassword.onclick = function(){makePasswordVisible('showRepeatPassword')}
 }
-
-/*let commandsDisplay = document.getElementById('commands')
-if(commandsDisplay !== null){
-    let commands = ['create', 'insert', 'delete']
-
-    for (let i = 0; i < commands.length; i++) {
-        let button = document.createElement('button')
-        button.innerHTML = commands[i]
-        button.onclick = function(){executeCommand(i)}
-        commandsDisplay.appendChild(button)
-    }
-
-    /*let exampleList = document.getElementById('exampleList')
-    
-    let listItems = document.getElementById('listItems')
-    
-    let exampleListItems = ['pizza', 'test', 'code']
-    
-    for (let i = 0; i < exampleListItems.length; i++) {
-        let li = document.createElement('li')
-        li.innerHTML = exampleListItems[i]
-        listItems.appendChild(li)
-    }
-
-    function executeCommand(i){
-        if(commands[i] == 'create'){
-            let listName = prompt('enter list name', 'name')
-            location.href = "home.php?listName="+listName
-        }
-        else if (commands[i] == 'delete'){
-
-        }
-        else if (commands[i] == 'insert'){
-            let listName = prompt('enter list name', 'name')
-            let listItem = prompt('enter list item content' , 'content')
-            location.href = "home.php?listName="+listName+"&listItem="+listItem
-        }
-    }
-}*/
 
 function makePasswordVisible(passwordType){
     if(passwordType == 'showPassword'){
@@ -109,44 +52,6 @@ else{
 }
 
 let listsDisplay = document.getElementById('listsDisplay')
-
-/*let lists = document.getElementById('lists')
-if(lists !== null){
-    if(lists.value !== null){
-        let listsDisplay = document.getElementById('listsDisplay')
-        console.log(lists.value)
-        lists.value = lists.value.replace("[{", "").replace("}]", "")
-        let arr = lists.value.split(',')
-        for (let i = 0; i < arr.length; i++) {
-            if(arr.indexOf(i) == -1){
-                arr.splice(i, 1)
-                arr[i] = arr[i].replace('"' + i + '":', "")
-            }
-        }
-        arr[2] = arr[2].replace('"[', "")
-        arr[2] = arr[2].replace(']"', "")
-        let userLists = arr[2].split('listName:')
-        for (let i = 1; i < userLists.length; i++) {
-            let ul = document.createElement('ul')
-            userLists[i] = userLists[i].replace('"', "")
-            userLists[i] = userLists[i].replace(']', "")
-            userLists[i] = userLists[i].replace(/\\/, "")
-            ul.id = userLists[i]
-            let ulTitle = document.createElement('h3')
-            ulTitle.innerHTML = userLists[i]
-            ul.appendChild(ulTitle)
-            listsDisplay.appendChild(ul)
-        }
-        console.log(userLists)
-    }
-}   
-let listItems = document.getElementById('listItems')
-if(listItems !== null){
-    console.log(listItems.value)
-}*/
-
-//list on click has to put id with it
-
 let userLists = document.getElementById('lists')
 let userListItems = document.getElementById('listItems')
 let commandsDisplay = document.getElementById('commands')
